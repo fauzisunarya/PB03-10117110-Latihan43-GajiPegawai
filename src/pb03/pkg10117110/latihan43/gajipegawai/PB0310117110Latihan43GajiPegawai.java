f9a16908c43ce17deed9bb0f4a4d89d658885d4e
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-10-18 14:58:53 +0700

    first commit

---
+++ src/pb03/pkg10117110/latihan43/gajipegawai/PB0310117110Latihan43GajiPegawai.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Gaji Pegawai Dengan Menggunakan Konsep OOP
+ */
+package pb03.pkg10117110.latihan43.gajipegawai;
+
+
+public class PB0310117110Latihan43GajiPegawai {
+
+
+
+    public static void main(String[] args) {
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+        
+    }
+    
+}
